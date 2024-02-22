110f7fa4053a7373e4250d70bcb84325e358151f
VKhmil <vovahmil@gmail.com>
2024-02-22 18:01:45 +0200

    Added
    Book entity representing a book with attributes such as title, author, ISBN, price, description, and cover image URL.
    Book service handling business logic related to managing books.
    Book repository providing CRUD operations for the Book entity.

--- src/main/java/mate/service/impl/BookServiceImpl.java
+++ src/main/java/mate/service/impl/BookServiceImpl.java
@@ -1,20 +1,16 @@
 package mate.service.impl;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import mate.model.Book;
 import mate.repository.BookRepository;
 import mate.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
-
-    @Autowired
-    public BookServiceImpl(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
 
     @Override
     public Book save(Book book) {
