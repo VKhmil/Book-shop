2b71a7d0fb0b2319dbcbcd6d027f9868e2ce90bb
VKhmil <vovahmil@gmail.com>
2024-03-02 19:25:52 +0200

    Add response and request DTOs for book handling
    Introduces BookDto.class as a response DTO and CreateBookRequestDto.class as a request DTO. Utilizes Mapstruct and mappers for seamless conversion between DTOs and entities. Implements DTO usage within the service layer, ensuring proper data handling.
    
    Additions include:
    - BookController with methods:
      - public List<BookDto> getAll()
      - public BookDto getBookById(Long id)
      - public BookDto createBook(CreateBookRequestDto bookDto)
    - Implementation of missing methods in BookService and BookRepository
    - Introduction of EntityNotFoundException class for handling exceptions
    - Removal of CommandLineRunner bean for cleaner codebase

--- src/main/java/mate/DemoApplication.java
+++ src/main/java/mate/DemoApplication.java
@@ -1,14 +1,10 @@
 package mate;
 
-import mate.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-    @Autowired
-    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
