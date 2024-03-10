b0e699b7bb51df1dd6206bda634198a282760f6c
VKhmil <vovahmil@gmail.com>
2024-03-10 22:59:44 +0200

    Implemented JPA Specifications
    Implemented a search endpoint allowing searches by author and title.
    
    In the repository layer, added a SpecificationBuilder, SpecificationProvider, SpecificationProviderManager, AuthorSpecificationProvider, and TitleSpecificationProvider.

--- src/main/java/mate/repository/book/BookRepository.java
+++ src/main/java/mate/repository/book/BookRepository.java
@@ -1,11 +1,12 @@
-package mate.repository;
+package mate.repository.book;
 
 import java.util.List;
 import mate.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
 
     @Query("FROM Book b WHERE UPPER(b.title) LIKE UPPER (:title)")
     List<Book> findAllByTitle(String title);
