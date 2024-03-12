c8a86b99a2a6d2b03829590097313f559816f816
VKhmil <68187475+VKhmil@users.noreply.github.com>
2024-03-12 18:42:07 +0200

    Revert "Added"

--- src/main/java/mate/dto/BookSearchParametersDto.java
+++
@@ -1,8 +0,0 @@
-package mate.dto;
-
-public record BookSearchParametersDto(String[] titles,
-                                      String[] authors,
-                                      String[] isbn,
-                                      String[] price,
-                                      String[] description) {
-}
