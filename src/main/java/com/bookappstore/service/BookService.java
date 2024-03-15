1cca51f992d7931d990b9e0578b424680c585d21
VKhmil <vovahmil@gmail.com>
2024-03-15 19:54:56 +0200

    Added pagination, sorting, add swagger(open APi)
    Used: Pageable, swagger annotation for describing controllers(API)
    Added: swagger dependency

--- src/main/java/com/bookappstore/service/BookService.java
+++ src/main/java/com/bookappstore/service/BookService.java
@@ -3,12 +3,13 @@ package com.bookappstore.service;
 import com.bookappstore.dto.BookDto;
 import com.bookappstore.dto.BookSearchParametersDto;
 import com.bookappstore.dto.CreateBookRequestDto;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto createBookRequestDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findById(Long id);
 
--- src/main/java/com/bookappstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/bookappstore/service/impl/BookServiceImpl.java
@@ -9,6 +9,7 @@ import com.bookappstore.model.Book;
 import com.bookappstore.repository.book.BookRepository;
 import com.bookappstore.repository.book.spec.BookSpecificationBuilder;
 import com.bookappstore.service.BookService;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
@@ -28,8 +29,8 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> findAll() {
-        return bookRepository.findAll().stream()
+    public List<BookDto> findAll(Pageable pageable) {
+        return bookRepository.findAll(pageable).stream()
                 .map(bookMapper::toDto)
                 .toList();
     }
