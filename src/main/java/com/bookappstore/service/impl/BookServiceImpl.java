5567fec46392c5787b53241255d696b40bffc80c
VKhmil <vovahmil@gmail.com>
2024-03-27 18:44:16 +0200

    Added pagination, and swagger

--- src/main/java/com/bookappstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/bookappstore/service/impl/BookServiceImpl.java
@@ -30,7 +30,7 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public List<BookDto> findAll(Pageable pageable) {
-        return bookRepository.findAll().stream()
+        return bookRepository.findAll(pageable).stream()
                 .map(bookMapper::toDto)
                 .toList();
     }
