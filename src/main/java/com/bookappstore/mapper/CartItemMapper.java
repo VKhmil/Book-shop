418c61148e276ff7c052c523443b1d2bfdae0188
VKhmil <vovahmil@gmail.com>
2024-04-28 23:32:13 +0300

    “Implemented Shopping Cart Functionality and User Use Cases”
    Pay attention, maybe I don't understand mentor's approach, please could you add more details, with respect, thanks

--- src/main/java/com/bookappstore/mapper/CartItemMapper.java
+++ src/main/java/com/bookappstore/mapper/CartItemMapper.java
@@ -2,7 +2,6 @@ package com.bookappstore.mapper;
 
 import com.bookappstore.config.MapperConfig;
 import com.bookappstore.dto.cart.CartItemRequestDto;
-import com.bookappstore.dto.cart.CartItemResponseDto;
 import com.bookappstore.dto.cart.CartItemUpdateDto;
 import com.bookappstore.model.Book;
 import com.bookappstore.model.CartItem;
@@ -10,7 +9,6 @@ import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.Mappings;
 
 @Mapper(config = MapperConfig.class)
 public interface CartItemMapper {
@@ -19,12 +17,6 @@ public interface CartItemMapper {
 
     @Mapping(target = "book", ignore = true)
     CartItem toEntity(CartItemUpdateDto cartItemUpdateDto);
-
-    @Mappings({
-            @Mapping(target = "bookId", source = "book.id"),
-            @Mapping(target = "bookTitle", source = "book.title")
-    })
-    CartItemResponseDto toDto(CartItem cartItem);
 
     @AfterMapping
     default void setBook(@MappingTarget CartItem cartItem,
--- src/main/java/com/bookappstore/model/CartItem.java
+++ src/main/java/com/bookappstore/model/CartItem.java
@@ -25,7 +25,7 @@ public class CartItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "shopping_cart_id")
+    @JoinColumn(name = "book_id", nullable = false)
     private ShoppingCart shoppingCart;
     @ManyToOne
     private Book book;
